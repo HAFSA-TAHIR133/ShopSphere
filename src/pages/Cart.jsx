890a376f00d01573f130c884eab67e7147ad502d
HAFSA_TAHIR13 <heerc838@mail.com>
2026-06-18 15:48:31 +0500

    git commit -m" Cart page ,add navbar & footer"

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,14 +1,25 @@
 import CartDropDown from "../components/cartDropDown";
 import { useNavigate } from "react-router-dom";
 import '../css/DropDown.css';
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
+
 function Cart(){
     const navigate =useNavigate();
+        
     return(
     <>
-    <div className="cart-page-container">
-        <button onClick={()=> navigate(-1)}>Go Back</button>
-        <CartDropDown variant="page"/>
-    </div>
+        <div className="home-layout">
+            <Navbar />
+            <div className="cart-page-container">
+                <button onClick={()=> navigate(-1)}>Go Back</button>
+                <CartDropDown variant="page"/>
+            </div>
+
+            {/* <Footer /> */}
+            <Footer />
+        </div>
     </>
     );
 }
